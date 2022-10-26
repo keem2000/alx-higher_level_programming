44689fed22371176f27eea571ded9b1e60d7891e
keem2000 <joycelinaomari@gmail.com>
2022-10-26 16:18:18 -0700

    i/o

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""Module for Student class"""
+
+"""Defines a class Student."""
 
 
 class Student:
-    """Representation of a Student"""
+    """Represent a student."""
     
     def __init__(self, first_name, last_name, age):
-        """Instantiation"""
+        """Initialize a new Student.
+ 
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         
     def to_json(self):
-        """Retrieves dictionary representation"""
-        return self.__dict__.copy()
+        """Get a dictionary representation of the Student."""
+        return self.__dict__
